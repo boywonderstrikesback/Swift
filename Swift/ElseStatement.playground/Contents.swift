7f31160b7a0360b6e2a0d0a23826c2eb51fe6680
Mike Ross <34175985+boywonderstrikesback@users.noreply.github.com>
2019-07-30 13:06:49 -0400

    Add files via upload

---
+++ Swift/ElseStatement.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+var g:Int = 800;
+
+if g < 60 {
+    
+    print("Value is less than 60");
+    
+} else {
+    
+    print("Value is not less than 60");
+    
+}
+
+print("Value of variable is \(g)");
---
+++ Swift/Hello.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+let ch1: Character = "G"
+
+let ch2: Character = "S"
+
+print ("First character is: \(ch1)")
+
+print ("Second character is: \(ch2)")
